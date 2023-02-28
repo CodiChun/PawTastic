c141e9dcc061913059760323ee6fb8a347229aac
Damien Cruz <dcruz99@uw.edu>
2023-02-28 23:38:01 +0000

    update config

---
+++ Appointment.php
@@ -0,0 +1,2 @@
+sdhgfgdbbfgbfgdb
+lkadlskddadsakjfgkjgjfgjgjgjjjjg
--- config.php
+++ config.php
@@ -3,4 +3,5 @@ define('DBHOST', 'localhost')
 define('DBUSER', 'testuser');
 define('DBPASS', 'mypassword');
 define('DBCONNSTRING','mysql:dbname=company;charset=utf8mb4');
+define('DNAME', 'pet_tastic');
 ?>
