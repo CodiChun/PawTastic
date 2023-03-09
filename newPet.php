3b0024c762fc5f8917459ccbb60ccf4259a70361
Damien Cruz <dcruz99@uw.edu>
2023-03-09 10:38:20 +0000

    started on add pet page

--- newPet.php
+++ newPet.php
@@ -1,3 +1,4 @@
+<?php require_once('config.php');?>
 <!DOCTYPE html>
 <html lang="en">
     <head>
@@ -52,6 +53,111 @@
     </nav>
     <!-- Nav Bar End-->
     <body>
+
+
+
+
+<!--
+INSERT INTO PET(Name, Owner_id, Breed, Birthday, Weight, Cut_Card, Microchipped, Rabies_Vaccine, Note) 
+VALUES ('Tarzan', 1001, 'Tabby', DATE '2011-1-11', 6, 2005, 'Yes', 'Yes', 'Friendly old orange cat');
+
+-->
+
+
+<h1>
+    Enter New Pet's Info
+        </h1>
+        <form id="leform" action="newPet.php" method = "POST">
+            <label for="petOwner" >Owner</label>
+            <select name="petOwner" id="petOwner" required >
+                <?php
+                $connection = mysqli_connect(DBHOST, DBUSER, DBPASS, DBNAME);
+                if ( mysqli_connect_errno() ){
+                    die( mysqli_connect_error() );
+                }
+                $sql = "SELECT * FROM PET_OWNER ORDER BY Name";
+                if ($result = mysqli_query($connection, $sql))
+                {
+                    // loop through the data
+                    while($row = mysqli_fetch_assoc($result))
+                    {
+                        echo '<option value="' . $row['Client_ID'] . '">';
+                        echo $row['Name']. ', '. $row['Client_ID'];
+                        echo "</option>";
+                    }
+                    // release the memory used by the result set
+                    mysqli_free_result($result);
+                }
+                ?>
+            </select>
+            <div class="form-group">
+                <label for="clname">Pets Name</label>
+                <input type="text" id="pname" name="pname" placeholder="Enter Pet's Name" required>
+
+
+                <label for="breed" class="form-label mt-4">Breed</label>
+                <input type="text" placeholder="Enter Animal Breed" required name = "breed">
+
+                <div>
+                    <label for="date">Date Of Birth:</label>
+                    <input type="date" id="Date" name="apptDate" required>
+                    <label for="weight" class="form-label mt-4" >Weight</label>
+                    <input type="number" id="weight" name="weight"
+                       placeholder="Enter Pet's weight"required>
+                </div>
+                <div>
+                    <label for="cutcard">Cut card</label>
+                    <input id="cutcard" name = "cutcard" type="number" placeholder="Assign Cut Card" required>
+                </div>
+                <div class="form-check">
+                    <input class="form-check-input" type="checkbox" id="chip">
+                    <label class="form-check-label" for="chip">
+                        Microchiped?
+                    </label>
+                </div>
+                <div class="form-check">
+                    <input class="form-check-input" type="checkbox" id="rab">
+                    <label class="form-check-label" for="rab">Vacinated Aganst Rabbies?</label>
+                </div>
+                <label>Notes</label>
+                <input id="notes" name = "notes" type="text" placeholder="Any Notes?" required>
+                <button type="submit" class="btn btn-primary">Submit</button>
+            </div>
+
+        </form>
+
+
+
+
+        <?php
+        $connection = mysqli_connect(DBHOST, DBUSER, DBPASS, DBNAME);
+        if ( mysqli_connect_errno() )
+        {
+            die( mysqli_connect_error() );
+        }
+        if ($_SERVER["REQUEST_METHOD"] == "POST")
+        {
+
+            if ( mysqli_connect_errno() )
+            {
+                die( mysqli_connect_error() );
+            }
+
+           // $sql = "INSERT INTO PET(Name, Owner_id, Breed, Birthday, Weight, Cut_Card, Microchipped, Rabies_Vaccine, Note)
+          //          VALUES ('Tarzan', 1001, 'Tabby', DATE '2011-1-11', 6, 2005, 'Yes', 'Yes', 'Friendly old orange cat');";
+           //echo $sql;
+            //$result = mysqli_query($connection, $sql);
+            //if (!$result) {
+              //  die('Error: ' . mysqli_error($connection));
+            //}
+
+            echo '<div class="alert alert-dismissible alert-success">
+  <strong>Client Added!!</strong><a href="newPet.php">would you like to attach a pet</a>
+</div>
+';
+        }
+        ?>
+
 
     </body>
 
